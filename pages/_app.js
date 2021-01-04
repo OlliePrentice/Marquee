a37c0f607947ecf5be0cda18f9cce89df903ae1d
Ollie Prentice <osprentice@gmail.com>
2021-01-04 22:25:39 +0000

    moved reflect

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,10 @@
-import "reflect-metadata";
 import '../styles/App.css'
 import 'swiper/swiper-bundle.min.css'
 import { Provider } from 'next-auth/client'
 import { config, library } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css';
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import "reflect-metadata";
 
 config.autoAddCss = false;
 library.add(fab);
